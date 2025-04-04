63a88ba7639d77aec5d802c3f30f66e046877a19
Andrew Yu-en Loh <loh6@ececomp2.ecn.purdue.edu>
2025-04-04 12:41:39 -0400

    version 1

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,84 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) { 
+  struct list_node* new_node = malloc(sizeof(struct list_node));
+  new_node -> value = value;
+  
+  return new_node; 
+}
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node* temp = new_node(value);
+  temp -> next = list -> head;
+  list -> head = temp;
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value) {
+  //case with empty list;
+  if(list -> head == NULL)
+  {
+    insert_at_head(list, value);
+  }
+  else
+  {
+    struct list_node* temp;
+    temp = list -> head;
+    while(temp -> next != NULL)
+    {
+      temp = temp -> next;
+    }
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+    struct list_node* tail = new_node(value);
+    temp -> next = tail;
+    tail -> next = NULL;
+  }
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+}
 
-void free_list(struct linked_list list) {}
+size_t remove_from_head(struct linked_list *list) { 
+  struct list_node* toDelete = list -> head;
+  size_t value = toDelete -> value;
+
+  list -> head = list -> head -> next;
+  free(toDelete);
+
+  return value; 
+}
+
+size_t remove_from_tail(struct linked_list *list) { 
+  size_t value;
+  
+  //case with one node
+  if(list -> head -> next == NULL)
+  {
+    value = remove_from_head(list);
+  }
+  else
+  {
+    struct list_node* prev = list -> head;
+    struct list_node* cur = prev -> next;
+    while(cur -> next != NULL)
+    {
+      prev = prev -> next;
+      cur = cur -> next;
+    }
+    value = cur -> value;
+    prev -> next = NULL;
+    free(cur);
+  }
+
+  return value; 
+}
+
+void free_list(struct linked_list list) {
+  while(list.head != NULL)
+  {
+    struct list_node* toFree = list.head;
+    list.head = list.head -> next;
+    free(toFree);
+  }
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,127 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+static bool checker(struct queue *q, struct game_state state);
+static bool same_tiles(struct game_state current, struct game_state toAdd);
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+//check whether you can enqueue
+static bool checker(struct queue *q, struct game_state state)
+{
+    size_t toAdd = serialize(state);
+    
+    //check if the state has not been entered
+    struct list_node* temp = (q -> data).head;
+    while(temp -> next != NULL)
+    {
+        if(temp -> value == toAdd)
+        {
+            return false;
+        }
+        if(same_tiles(deserialize(temp -> value), state))
+        {
+            return false;
+        }
+        temp = temp -> next;
+    }
 
-int number_of_moves(struct game_state start) { return 0; }
+    if(temp -> value != toAdd && same_tiles(deserialize(temp -> value), state))
+    {
+        return false;
+    }
+    return true;
+
+}
+
+//check whether the tiles of two game states are the same
+static bool same_tiles(struct game_state currentState, struct game_state toCompare)
+{
+    for(int r = 0; r < 4; r++)
+    {
+        for(int c = 0; c < 4; c++)
+        {
+            if(currentState.tiles[r][c] == toCompare.tiles[r][c])
+            {
+                return true;
+            }
+        }
+    }
+    
+    return false;
+
+}
+
+
+void enqueue(struct queue *q, struct game_state state) {
+    size_t toAdd = serialize(state);
+    insert_at_tail(&(q -> data), toAdd);
+
+    return;
+    
+}
+
+struct game_state dequeue(struct queue *q) { 
+    size_t toCompare = remove_from_head(&(q -> data));
+
+    return deserialize(toCompare); 
+}
+
+int number_of_moves(struct game_state start) {
+    struct queue q;
+
+    struct game_state complete;
+
+    //initialize here
+    complete.empty_row = 0;
+    complete.empty_col = 0;
+    complete.num_steps = 0;
+    int counter = 1;
+    for(int r = 0; r < 4; r++)
+    {
+        for(int c = 0; c < 4; c++)
+        {
+            complete.tiles[r][c] = counter;
+            counter++;
+        }
+    }
+    complete.tiles[4][4] = 0;
+
+    if(checker(&q, start))
+    {
+        enqueue(&q, start);
+    }
+    
+    while(q.data.head -> next != NULL)
+    {
+        struct game_state temp = dequeue(&q);
+        if(same_tiles(temp, complete))
+        {
+            return temp.num_steps;
+        }
+        else
+        {
+            struct game_state left = temp; 
+            move_left(&temp);
+            left.num_steps++;
+
+            struct game_state right = temp; 
+            move_right(&temp);
+            right.num_steps++;
+
+            struct game_state up = temp; 
+            move_up(&temp);
+            up.num_steps++;
+
+            struct game_state down = temp; 
+            move_down(&temp);
+            down.num_steps++;
+
+            enqueue(&q, left);
+            enqueue(&q, right);
+            enqueue(&q, up);
+            enqueue(&q, down);
+        }
+
+    }
+    
+    return -1; 
+}
